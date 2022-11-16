e2273ea2e2f8df7bd811783bf55e005d452004ee
Fesposito9197 <kekkoesp8@gmail.com>
2022-11-16 16:11:42 +0100

    aggiunte vue js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,48 @@
-function addPartecipant(partecipantName){
+// function addPartecipant(partecipantName){
 
-}
-const genera = document.getElementById('add-hero');
-let partecipantName = document.getElementById("partecipant");
-let partecipantsList = document.querySelector(".partecipants-list");
+// }
+// const genera = document.getElementById('add-hero');
+// let partecipantName = document.getElementById("partecipant");
+// let partecipantsList = document.querySelector(".partecipants-list");
 
-genera.addEventListener('click' , function(){
+// genera.addEventListener('click' , function(){
    
-   if (partecipants.length < 2) {
-       partecipants.push(partecipantName.value);
-       let listItem = document.getElementById("template-list-item").content.cloneNode(true);
-       listItem.querySelector('.partecipants-list-item').innerHTML= partecipantName.value
-       partecipantsList.append(listItem)
-    }else{
-        alert('TESTA A CERBOTTANA SIAMO GIA ARRIVATI A 16')
+//    if (partecipants.length < 2) {
+//        partecipants.push(partecipantName.value);
+//        let listItem = document.getElementById("template-list-item").content.cloneNode(true);
+//        listItem.querySelector('.partecipants-list-item').innerHTML= partecipantName.value
+//        partecipantsList.append(listItem)
+//     }else{
+//         alert('TESTA A CERBOTTANA SIAMO GIA ARRIVATI A 16')
+//     }
+//     partecipantName .value = "";
+// })
+
+// let partecipants = [];
+
+// let partecipant = {
+//     name :'titolo',
+//     chatName : 'PowerMuffin',
+//     icon: 'url'
+// }
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        partecipants: [
+            
+        ],
+        partecipanti:''
+      }
+    },
+    methods: {
+        addPartecipant() {
+            this.partecipants.push(this.partecipanti)
+            this.partecipanti=''
+        }
     }
-    partecipantName .value = "";
-})
-
-let partecipants = [];
-
-let partecipant = {
-    name :'titolo',
-    chatName : 'PowerMuffin',
-    icon: 'url'
-}
-   
-        
+  }).mount('#app')
        
    
     
